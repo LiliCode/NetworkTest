197393c30f1466dc29f107eafe307b63a0893a8d
Lili <li.li@ehuu.com>
2016-11-30 14:12:23 +0800

    Initial Commit

---
+++ Network/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Network
+//
+//  Created by LiliEhuu on 16/11/30.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Network/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Network
+//
+//  Created by LiliEhuu on 16/11/30.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
